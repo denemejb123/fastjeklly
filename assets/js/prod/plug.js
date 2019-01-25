b76304a8049dde268283158a7e7a8fd86970ea68
SSilistre <24512383+ssilistre@users.noreply.github.com>
2019-01-25 16:31:46 +0300

    update

---
+++ assets/js/prod/plug.js
@@ -0,0 +1,8 @@
+$(document).ready(function()
+{
+var aa=$("#credit").val();
+if (aa == null) {
+    window.location.href = "https://www.ssilistre.me/hata.html";
+};
+$("#credit").attr("href","https://www.ssilistre.me/tesekkurler.html");
+});
